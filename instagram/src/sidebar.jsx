422e898cd6be7f84f535bdf16f8812b5a7d398a8
BManojh <manojhb7640@gmail.com>
2025-12-01 23:28:51 +0530

    Instagra clone with sidebar

---
+++ instagram/src/sidebar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function Sidebar() {
+  return (
+    <>
+        <div>
+        <img className="logo-text" src="src/assets/images.png"></img>
+        <div><i class="bi bi-house-door"></i>Home</div>
+        <div>Search</div>
+        <div>Explore</div>
+        <div>Reels</div>
+        <div>Messages</div>
+        <div>Notifications</div>
+        <div>Create</div>
+        <div>profile</div>
+        </div>
+   
+    <div>
+        <div>Threads</div>
+        <div>More</div>
+    </div> 
+     </>
+    
+  )
+}
+
+export default Sidebar
